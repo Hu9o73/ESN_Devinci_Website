f6a1d59bd51171bc5b22c1059489f19be3dff4c0
Hugo BONNELL <hbonnell@live.fr>
2024-12-26 02:08:51 +0100

    Wacky tests for online deployment

--- Frontend/src/main.server.ts
+++
@@ -1,7 +0,0 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './app/Components/BaseComponent/app.component';
-import { config } from './app/Config/app.config.server';
-
-const bootstrap = () => bootstrapApplication(AppComponent, config);
-
-export default bootstrap;
